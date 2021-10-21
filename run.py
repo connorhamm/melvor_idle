5886540a4ea2ab10fca97685f2851cebe432209c
connorhamm <connorjhamm@gmail.com>
2021-10-21 15:25:43 -0700

    Using Class Format

--- run.py
+++ run.py
@@ -10,9 +10,9 @@ Select better healing items - complete today if its fun to do so
 2.  test functionality - pending
 
 Auto Heal - Step 1 - complete today if its fun to do so
-1. Add template for wave complete / died / ok
-2. Implement template to only move into equipment phase if wave complete template is detected
-3. Test feature
+1. Add template for wave complete / died / ok - pending new screenshot and monitor dimensions
+2. Implement template to only move into equipment phase if wave complete template is detected - done
+3. Test feature - pending
 
 Auto Heal - Step 2 
 1. Change picture position for health bar
@@ -35,19 +35,16 @@ import time
 import numpy as np
 from datetime import datetime
 
-# returns image template
 def img_template(file):
     template = cv2.imread(file)
     template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
     template = cv2.Canny(template, 50, 200)
     return template
 
-# returns image to run comparison for matchTemplate
 def take_image(monitor, screenshot):
     with mss() as sct:
         img = sct.grab(monitor)
 
-        # Take Screenshot for debugging purposes
         if screenshot == True:
             output = str(datetime.now())[:5:1]+ ".png"
             tools.to_png(img.rgb, img.size, output=output)
@@ -58,8 +55,6 @@ def take_image(monitor, screenshot):
         edged = cv2.Canny(gray, 50, 200)
         return edged
 
-# returns True/False if threshold is met
-# returns location variable
 def matchTemplate(img_template, edged, threshold):
         result = cv2.matchTemplate(edged, img_template, cv2.TM_CCOEFF_NORMED)
         (_, maxVal, _, maxLoc) = cv2.minMaxLoc(result)
@@ -83,73 +78,126 @@ def matched_click(loc, w, h):
     pyautogui.moveTo(10,10)
     time.sleep(1)
 
+#################### State Machine ############################
+class idle_bot:
+    def __init__(self):
+        # Set monitor detection WxH and offsets
+        self.mon = {'top': 0, 'left': 0, 'width': 1920, 'height': 1080}
+
+        # Set image template
+        self.armour_template = img_template("armour.PNG")
+        self.w1, self.h1 = self.armour_template.shape[::-1]
+
+        self.ok_template = img_template("ok.PNG")
+        self.w2, self.h2 = self.ok_template.shape[::-1]
+
+        self.food_template = img_template("food.PNG")
+        self.w3, self.h3 = self.food_template.shape[::-1]
+
+        self.one_x_template = img_template("1x.PNG")
+        self.w4, self.h4 = self.food_template.shape[::-1]
+
+        self.start_raid_template = img_template("start_raid.PNG")
+        self.w5, self.h5 = self.start_raid_template.shape[::-1]
+
+        # self.good_food_template = img_template("good_food.PNG")
+        # self.w6, self.h6 = self.good_food_template.shape[::-1]
+
+        self.wave_complete_template = img_template("wave_complete.PNG")
+        self.w7, self.h7 = self.wave_complete_template.shape[::-1]
+
+        print("Init Complete")
+        self.queue()
+
+    def queue(self):
+        print("In Queue")
+        time.sleep(3)
+        while(1):
+            edged_img = take_image(self.mon,False)
+            ok, loc = matchTemplate(self.ok_template, edged_img, 0.9)
+            if ok == True:
+                print("Event: Clicking OK")
+                matched_click(loc, self.w1, self.h1)  
+            else:
+                start_raid, start_raid_loc = matchTemplate(self.start_raid_template, edged_img, 0.9)
+                if start_raid == True:
+                    print("Event: Starting Raid!")
+                    matched_click(start_raid_loc, self.w5, self.h5)
+                    self.combat()
+
+    def combat(self):
+        while(1):
+            print("Auto-eat")
+            edged_img = take_image(self.mon,False)
+
+
+    def equipment(self):
+        print("selecting equipment")
+
+idle_bot()
+
+
 ################################ MAIN CODE #####################################
 
-# Set monitor detection WxH and offsets
-mon = {'top': 0, 'left': 0, 'width': 1920, 'height': 1080}
 
-# Set image template
-armour_template = img_template("armour.PNG")
-w1, h1 = armour_template.shape[::-1]
 
-ok_template = img_template("ok.PNG")
-w2, h2 = ok_template.shape[::-1]
 
-food_template = img_template("food.PNG")
-w3, h3 = food_template.shape[::-1]
+# # Set monitor detection WxH and offsets
+# mon = {'top': 0, 'left': 0, 'width': 1920, 'height': 1080}
 
-one_x_template = img_template("1x.PNG")
-w4, h4 = food_template.shape[::-1]
+# # Set image template
+# armour_template = img_template("armour.PNG")
+# w1, h1 = armour_template.shape[::-1]
 
-start_raid_template = img_template("start_raid.PNG")
-w5, h5 = start_raid_template.shape[::-1]
+# ok_template = img_template("ok.PNG")
+# w2, h2 = ok_template.shape[::-1]
 
-good_food_template = img_template("good_food.PNG")
-w6, h6 = good_food_template.shape[::-1]
+# food_template = img_template("food.PNG")
+# w3, h3 = food_template.shape[::-1]
 
-# Counters
-armour_cnt = 0
+# one_x_template = img_template("1x.PNG")
+# w4, h4 = food_template.shape[::-1]
 
-# State Machine
-time.sleep(3)
+# start_raid_template = img_template("start_raid.PNG")
+# w5, h5 = start_raid_template.shape[::-1]
 
-while(1):
-    # In combat, stay in combat loop until death or round over is detected or ok button
-    
-    
+# good_food_template = img_template("good_food.PNG")
+# w6, h6 = good_food_template.shape[::-1]
 
-    edged_img = take_image(mon,False)
-    armour, armour_loc = matchTemplate(armour_template, edged_img, 0.9)
-    food, food_loc = matchTemplate(food_template, edged_img, 0.9)
+# wave_complete_template = img_template("wave_complete.PNG")
+# w7, h7 = wave_complete_template.shape[::-1]
 
-    if armour == True and armour_cnt < 5:
-        print("Event: Selecting Armour")
-        matched_click(armour_loc, w1, h1)
-        time.sleep(1)
-        edged_img = take_image(mon,False)
-        item, item_loc = matchTemplate(one_x_template, edged_img, 0.5)
-        matched_click(item_loc, w4, h4)
-        armour_cnt += 1
+# # Counters
+# armour_cnt = 0
 
-    elif armour == True and armour_cnt >= 5:
-        print("Event: Selecting Food")
-        matched_click(food_loc, w3, h3)
-        time.sleep(1)
-        edged_img = take_image(mon,False)
-        item, item_loc = matchTemplate(good_food_template, edged_img, 0.5)
-        if item == True:
-            matched_click(item_loc, w4, h4)
-        else:
-            item, item_loc = matchTemplate(one_x_template, edged_img, 0.5)
-            matched_click(item_loc, w6, h6)
-        armour = 0
-    
-    ok, loc = matchTemplate(ok_template, edged_img, 0.9)
-    if ok == True:
-        print("Event: You Died!")
-        matched_click(loc, w1, h1)
+# while(1):
+#     wave_complete = False
+#     edged_img = take_image(mon,False)
 
-    start_raid, start_raid_loc = matchTemplate(start_raid_template, edged_img, 0.9)
-    if start_raid == True:
-        print("Event: Starting Raid!")
-        matched_click(start_raid_loc, w5, h5)
+#     # Check if dead or in equipment state
+#     wave_complete, _ = matchTemplate(wave_complete_template, edged_img, 0.9)
+#     if wave_complete == True:
+#         armour, armour_loc = matchTemplate(armour_template, edged_img, 0.9)
+#         food, food_loc = matchTemplate(food_template, edged_img, 0.9)
+
+#         if armour == True and armour_cnt < 5:
+#             print("Event: Selecting Armour")
+#             matched_click(armour_loc, w1, h1)
+#             time.sleep(1)
+#             edged_img = take_image(mon,False)
+#             item, item_loc = matchTemplate(one_x_template, edged_img, 0.5)
+#             matched_click(item_loc, w4, h4)
+#             armour_cnt += 1
+
+#         elif armour == True and armour_cnt >= 5:
+#             print("Event: Selecting Food")
+#             matched_click(food_loc, w3, h3)
+#             time.sleep(1)
+#             edged_img = take_image(mon,False)
+#             item, item_loc = matchTemplate(good_food_template, edged_img, 0.5)
+#             if item == True:
+#                 matched_click(item_loc, w4, h4)
+#             else:
+#                 item, item_loc = matchTemplate(one_x_template, edged_img, 0.5)
+#                 matched_click(item_loc, w6, h6)
+#             armour = 0
