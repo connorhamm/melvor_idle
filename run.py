f5f7f15c8c4dd2c47750a0a1f951836a400e7724
connorhamm <connorjhamm@gmail.com>
2021-10-21 14:43:31 -0700

    Added next steps for healing

--- run.py
+++ run.py
@@ -1,15 +1,30 @@
 """
 Main issue:
-- Miss clicks on food/armour due to the text box being different sizes based on the item
 - Doesn't select good items w/ damage reduction
-- Doesn't auto heal when health is too low
-- You want fast reaction time to health bar, so when running will need a seperate check, this feature will be added later
+- Doesn't select good healing items w/ high health recovery
+- Doesn't auto heal when health is too low, this what causes early deaths ()
+- Doesn't select good weapon
 
-6. Add in a template for all DR equipment
-7. Add in a template for all best weapons
-8. Add in a template for all best food
-9. Implement a new feature for checking on health percentage - pending
-    - If bar falls below 60% then eat
+Select better healing items - complete today if its fun to do so
+1.  Add template for items with 2x 0's in HP recovery. - pending screenshot
+2.  test functionality - pending
+
+Auto Heal - Step 1 - complete today if its fun to do so
+1. Add template for wave complete / died / ok
+2. Implement template to only move into equipment phase if wave complete template is detected
+3. Test feature
+
+Auto Heal - Step 2 
+1. Change picture position for health bar
+2. Add template for target health missing
+3. Test feature to ensure health missing is detected
+4. Find location of where to click for healing
+5. Add template for click to heal
+6. Implement click to heal feature
+
+DR Reduction
+1. Look into method for being able to check if good item is already equipped
+2. Create list of best in slot melee gear if best in slot is not available, select something else.
 """
 
 import cv2
@@ -89,6 +104,9 @@ w4, h4 = food_template.shape[::-1]
 start_raid_template = img_template("start_raid.PNG")
 w5, h5 = start_raid_template.shape[::-1]
 
+good_food_template = img_template("good_food.PNG")
+w6, h6 = good_food_template.shape[::-1]
+
 # Counters
 armour_cnt = 0
 
@@ -96,10 +114,14 @@ armour_cnt = 0
 time.sleep(3)
 
 while(1):
-    edged_img = take_image(mon,False)
+    # In combat, stay in combat loop until death or round over is detected or ok button
+    
+    
 
+    edged_img = take_image(mon,False)
     armour, armour_loc = matchTemplate(armour_template, edged_img, 0.9)
     food, food_loc = matchTemplate(food_template, edged_img, 0.9)
+
     if armour == True and armour_cnt < 5:
         print("Event: Selecting Armour")
         matched_click(armour_loc, w1, h1)
@@ -114,8 +136,12 @@ while(1):
         matched_click(food_loc, w3, h3)
         time.sleep(1)
         edged_img = take_image(mon,False)
-        item, item_loc = matchTemplate(one_x_template, edged_img, 0.5)
-        matched_click(item_loc, w4, h4)
+        item, item_loc = matchTemplate(good_food_template, edged_img, 0.5)
+        if item == True:
+            matched_click(item_loc, w4, h4)
+        else:
+            item, item_loc = matchTemplate(one_x_template, edged_img, 0.5)
+            matched_click(item_loc, w6, h6)
         armour = 0
     
     ok, loc = matchTemplate(ok_template, edged_img, 0.9)
